f30b56259775d7efd7a3a9e41f3c2e74a576c875
svijay77 <sarvagnyav@gmail.com>
2022-07-14 21:39:56 -0500

    Fix NL bar to fit screen and horizontal scroll issue

--- arpeggia-website/src/components/NlText.js
+++ arpeggia-website/src/components/NlText.js
@@ -6,16 +6,8 @@ export default function NlText() {
     <div className="nl-text flex-col-hstart-vstart">
       <div className="group-392">
         <p className="txt-3310 flex-hcenter">Sign up for our Newsletter! </p>
-      </div>
-      <p className="txt-737">
-        Learn more about what our musicians and mentors have been up to, as well
-        as information on upcoming events and programs.
-      </p>
-      <div>
-        <p className="txt-3310 flex-hcenter" id="iAmA">I am a... </p>
-      </div>
-      <div id= "options">
-      <div className="group-262 flex-row">
+        <div id= "options">
+        <div className="group-262 flex-row">
           <div className="check-1" />
           <p className="txt-912 flex-hcenter">Educator</p>
         </div>
@@ -24,17 +16,23 @@ export default function NlText() {
           <p className="txt-914 flex-hcenter">Student</p>
         </div>
         <div className="group-4411 flex-row-vend">
-        <div className="check-2" />
-        <p className="txt-914 flex-hcenter">Parent</p>
+          <div className="check-2" />
+          <p className="txt-914 flex-hcenter">Parent</p>
+        </div>
+        <div className="email-bar flex-col-hstart-vstart">
+          <div className="group-371">
+            <p className="txt-2108 flex-hcenter">email address</p>
+          </div>
+        </div>
       </div>
-      <div className="email-bar flex-col-hstart-vstart">
-      <div className="group-371">
-        <p className="txt-2108 flex-hcenter">email address</p>
       </div>
-    </div>
+      <p className="txt-737">
+        Learn more about what our musicians and mentors have been up to, as well
+        as information on upcoming events and programs.
+      </p>
+      <div>
+        <p className="txt-3310 flex-hcenter" id="iAmA">I am a... </p>
       </div>
- 
-
     </div>
   )
 }
