4653b3387ed03e3febdfc8df3c4bc617c9692dc8
svijay77 <sarvagnyav@gmail.com>
2022-07-16 14:58:59 -0500

    Add routing functionality to Our Team page

--- arpeggia-website/src/App.js
+++ arpeggia-website/src/App.js
@@ -2,14 +2,36 @@ import React from "react"
 import NavBar from "./Components/NavBar"
 import MainBanner from "./Components/MainBanner"
 import NlText from "./Components/NlText"
+import { Routes, Route} from "react-router";
 
 export default function App() {
   return (
   <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path = "/aboutus" element={<AboutUs />} /> */}
+      <Route path = "/ourteam" element={<Team />} />
+      {/* <Route path = "/involve" element={<Involve />} />  */}
+    </Routes>
+  </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
       <NavBar />
       <MainBanner />
       <NlText />
-  </div>
+    </div>
+  )
+}
 
-  );
+function Team() {
+  return (
+    <div>
+      <NavBar />
+      <p>Welcome to our team lol!</p>
+    </div>
+  )
 }
--- arpeggia-website/src/components/NavBar.js
+++ arpeggia-website/src/components/NavBar.js
@@ -1,16 +1,17 @@
 import React from "react"
 import "./styles.css"
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
       <div className="group-764 flex-row-vcenter">
-        <img
+        <Link to = "/"><img
           src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/fb518oktr7c-152%3A21?alt=media&token=a731d736-ca01-4965-ba67-bb679636c3e7"
           alt="Not Found"
           className="arpeggia-_logo-_transparent-011"
-        />
+        ></img></Link>
         <p className="txt-777 flex-hcenter"> About Us</p>
-        <p className="txt-476 flex-hcenter">Our Team</p>
+        <Link to = "/ourteam" className="txt-476 flex-hcenter">Our Team</Link>
         <p className="txt-387 flex-hcenter">Get Involved</p>
       </div>
   )
