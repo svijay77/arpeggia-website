59aa99af7923fa2383b58a837417c39a62ff01e0
svijay77 <sarvagnyav@gmail.com>
2022-07-15 23:54:40 -0500

    Fix horizontal overflow scroll issue

--- arpeggia-website/src/App.js
+++ arpeggia-website/src/App.js
@@ -9,7 +9,6 @@ export default function App() {
       <NavBar />
       <MainBanner />
       <NlText />
-
   </div>
 
   );
--- arpeggia-website/src/components/NlText.js
+++ arpeggia-website/src/components/NlText.js
@@ -6,33 +6,34 @@ export default function NlText() {
     <div className="nl-text flex-col-hstart-vstart">
       <div className="group-392">
         <p className="txt-3310 flex-hcenter">Sign up for our Newsletter! </p>
-        <div id= "options">
-        <div className="group-262 flex-row">
-          <div className="check-1" />
-          <p className="txt-912 flex-hcenter">Educator</p>
-        </div>
-        <div className="group-441 flex-row-vend">
-          <div className="check-2" />
-          <p className="txt-914 flex-hcenter">Student</p>
-        </div>
-        <div className="group-4411 flex-row-vend">
-          <div className="check-2" />
-          <p className="txt-914 flex-hcenter">Parent</p>
-        </div>
-        <div className="email-bar flex-col-hstart-vstart">
-          <div className="group-371">
-            <p className="txt-2108 flex-hcenter">email address</p>
-          </div>
-        </div>
-      </div>
-      </div>
-      <p className="txt-737">
+        <p className="txt-737">
         Learn more about what our musicians and mentors have been up to, as well
         as information on upcoming events and programs.
-      </p>
-      <div>
-        <p className="txt-3310 flex-hcenter" id="iAmA">I am a... </p>
+        </p>
       </div>
+      <div className= "options">
+          <p className="txt-3310 flex-hcenter" id="iAmA">I am a... </p>
+          <div className="choices">
+            <div className="group-262 flex-row">
+              <div className="check-1" />
+              <p className="txt-912 flex-hcenter">Educator</p>
+            </div>
+            <div className="group-441 flex-row-vend">
+              <div className="check-2" />
+              <p className="txt-914 flex-hcenter">Student</p>
+            </div>
+            <div className="group-4411 flex-row-vend">
+              <div className="check-2" />
+              <p className="txt-914 flex-hcenter">Parent</p>
+            </div>
+          </div>
+          
+          <div className="email-bar flex-col-hstart-vstart">
+            <div className="group-371">
+              <p className="txt-2108 flex-hcenter">email address</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
