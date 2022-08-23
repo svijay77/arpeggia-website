77c8095965f53cc42f3a7cdae06179472c2a2f5a
Saumya Madhan <smadhan2@illinois.edu>
2022-08-23 13:51:16 -0500

    Adjusted element sizing and scale.

--- src/Components/NavBarBlue.js
+++ src/Components/NavBarBlue.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 export default function NavBarBlue() {
   return (
-      <div className="group-765 flex-row-vcenter">
-        <Link to = "/"><img
-          src="https://i.postimg.cc/PJqWZMH3/image.png"
-          alt="Not Found"
-          className="arpeggia-_logo-_transparent-013"
-        /></Link>
-        <p className="txt-778 flex-hcenter"> About Us</p>
-        <Link to = "/ourteam" className="txt-477 flex-hcenter">Our Team</Link>
-        <Link to = "/involve"className="txt-388 flex-hcenter">Get Involved</Link>
-      </div>
+    <div className="group-765 flex-row-vcenter">
+    <Link to = "/"><img
+      src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/f934c5zjpp-241%3A16?alt=media&token=4c8831d4-93ff-4bd3-a854-f1364b7f012e"
+      alt="Not Found"
+      className="arpeggia-_logo-_transparent-013"
+    /></Link>
+    <p className="txt-777 flex-hcenter"> About Us</p>
+    <Link to = "/ourteam" className="txt-476 flex-hcenter">Our Team</Link>
+    <Link to = "/involve"className="txt-387 flex-hcenter">Get Involved</Link>
+  </div>
   )
 }
