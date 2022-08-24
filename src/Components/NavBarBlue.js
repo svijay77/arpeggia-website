3a727b22fe87887719e772b83c1e3f1f2c4e874f
Saumya Madhan <smadhan2@illinois.edu>
2022-08-24 17:44:09 -0500

    change text color to black

--- src/Components/NavBarBlue.js
+++ src/Components/NavBarBlue.js
@@ -6,9 +6,9 @@ export default function NavBarBlue() {
   return (
     <div className="group-765 flex-row-vcenter">
     <Link to = "/"><img
-      src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/f934c5zjpp-241%3A16?alt=media&token=4c8831d4-93ff-4bd3-a854-f1364b7f012e"
+      src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/xbc99eaf5as-241%3A19?alt=media&token=edb74612-4d39-4851-8844-8a8199d3eb23"
       alt="Not Found"
-      className="arpeggia-_logo-_transparent-013"
+      className=".arpeggia-_logo-_transparent-013"
     /></Link>
     <p className="txt-777 flex-hcenter"> About Us</p>
     <Link to = "/ourteam" className="txt-476 flex-hcenter">Our Team</Link>
